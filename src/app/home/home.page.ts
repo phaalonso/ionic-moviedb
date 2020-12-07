7e089cf0942c7718540bfe9cc84ff4a826003ba9
phaalonso <phaalonso@gmail.com>
2020-12-07 11:13:04 -0300

    MovieDB

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { SubjectSubscriber } from 'rxjs/internal/Subject';
+import { FilmeService } from '../services/filme.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,78 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public listaFilmes = [];
+  public pagina = 1;
+  // public pagina = 498; //Para testar o toast na ultima pagina
+  public totalPages: number;
+  public genres = {};
+
+  constructor(public filmeService: FilmeService, public toastController: ToastController, public loadingController: LoadingController) {
+    this.carregarFilmes();
+  }
+
+  async loading() {
+    const loading = await this.loadingController.create({
+      message: 'Carregando',
+      translucent: true,
+      showBackdrop: false,
+    });
+
+    loading.present();
+    return loading;
+  }
+
+  async mostrarToast(mensagem: string) {
+    const toast = await this.toastController.create({
+      message: mensagem,
+      duration: 1000
+    });
+
+    toast.present();
+  }
+
+  proximo() {
+    if (this.pagina != this.totalPages) {
+      this.pagina++;
+      this.carregarFilmes();
+    } else {
+      this.mostrarToast('Ultima página!');
+    }
+  }
+
+  anterior() {
+    if (this.pagina > 1) {
+      this.pagina--;
+      this.carregarFilmes();
+    } else {
+      this.mostrarToast('Primeira página!');
+    }
+  }
+
+  carregarFilmes() {
+    /* 
+    * Tinha usado um negócio para mostrar que a página está carregando, no entanto
+    * no entanto por carregar rápido ficou bem estranho
+    */
+    this.loading().then((loading) => {
+      this.filmeService.getMovies(this.pagina).subscribe((sucess: any) => {
+        console.log(sucess);
+        this.listaFilmes = sucess.results.map(rs => {
+          rs.release_date = new Date(rs.release_date).toLocaleDateString('pt-BR')
+          return rs;
+        });
+        this.totalPages = sucess.total_pages;
+        loading.dismiss();
+        document.querySelector('ion-content').scrollToTop(0);
+      }, error => {
+        console.log(error);
+        loading.dismiss();
+      });
+      })
+    }
+
+  formatFilmUrl(backdrop_path) {
+      return `https://image.tmdb.org/t/p/w500/${backdrop_path}`;
+    }
 
 }
--- src/app/services/filme.service.ts
+++ src/app/services/filme.service.ts
@@ -1,12 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmeService {
 
-  private baseURI = '';
+  private baseURI = 'https://api.themoviedb.org/3';
 
   constructor(private http: HttpClient) { }
+
+  public getMovies(page: number) {
+    return this.http.get(`${this.baseURI}/movie/popular?api_key=${environment.movieDB}&page=${page}`);
+  } 
+
+  public getGenresForMovies() {
+    return this.http.get(`${this.baseURI}/genre/movie/list?api_key=${environment.movieDB}`)
+  }
+  
 }
